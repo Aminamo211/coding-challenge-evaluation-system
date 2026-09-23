9890d671a1c305828912aeb60f93b3758e42048d
Aminamoh <moham210@csusm.edu>
2026-09-22 19:29:13 -0700

    Add the testCase.

---
+++ src/TestCase.java
@@ -0,0 +1,23 @@
+public class TestCase {
+    private String input;
+    private String expectedOutput;
+
+    public TestCase(String input, String expectedOutput) {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public String getExpectedOutput() {
+        return expectedOutput;
+    }
+
+    @Override
+    public String toString() {
+        return "Input: " + input + ", Expected Output: " + expectedOutput;
+    }
+}
+
